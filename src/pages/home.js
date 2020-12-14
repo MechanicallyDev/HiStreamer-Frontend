2b68469d6d35ec3729c7291a4ea808d5dbf47714
MechanicallyDev <contato@mechanically.dev>
2020-12-14 16:09:23 -0300

    removed background image on default home header

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,7 +9,6 @@ const Home = () => {
         hero="true"
         color="#fff"
         backgroundColor="#00000022"
-        backgroundImage="https://loremflickr.com/1920/500"
         minHeight="40vh"
       />
       <PageTemplate title="Home"></PageTemplate>
