f7ae62fec6fdec76cbe0614bff835c92b8d6cd35
MechanicallyDev <contato@mechanically.dev>
2020-12-25 02:29:10 -0300

    added API proxy fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://histreamer.com:3333' });
+const api = axios.create({ baseURL: 'http://histreamer.com/api/' });
 api.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 api.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
