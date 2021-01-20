a01d10595fbbb9e3f58bd7c59fa6249a3bdddff9
Bruno <sputnicker@gmail.com>
2021-01-20 14:40:34 -0300

    added validation on contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,45 +1,129 @@
-import PageTemplate from 'layouts/PageTemplate';
-import styled from 'styled-components';
-import Header from 'components/Header';
+import React from "react";
+import PageTemplate from "layouts/PageTemplate";
+import styled from "styled-components";
+import Header from "components/Header";
 
 const Contact = () => {
+  const [hasSent, setHasSent] = React.useState(false);
+  const [name, setName] = React.useState("");
+  const [subject, setSubject] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [hasError, setHasError] = React.useState(false);
+  const [errors, setErrors] = React.useState([]);
+
+  function validateForm() {
+    function validateEmail(email) {
+      var re = /\S+@\S+\.\S+/;
+      return re.test(email);
+    }
+    function minSize(field, size) {
+      return field.trim().length <= size;
+    }
+    function maxSize(field, size) {
+      return field.trim().length >= size;
+    }
+    let errorList = [];
+    if (minSize(name, 3) || maxSize(name, 145)) {
+      errorList = ["name", ...errorList];
+    }
+    if (!validateEmail(email) || minSize(email, 5)) {
+      errorList = ["email", ...errorList];
+    }
+    if (minSize(subject, 3) || maxSize(subject, 145)) {
+      errorList = ["subject", ...errorList];
+    }
+    if (minSize(message, 20)) {
+      errorList = ["message", ...errorList];
+    }
+    if (errorList.length > 0) {
+      setHasError(true);
+      setErrors(errorList);
+      return true;
+    } else {
+      setHasError(false);
+      return false;
+    }
+  }
+
+  async function handleFormSubmition(e) {
+    e.preventDefault();
+    if (validateForm()) return;
+    setErrors([])
+    setHasSent(true);
+  }
+
   return (
     <PageTemplate title="Contact">
       <Header
         title="Contact Us"
         description={[
-          'Do you want a new feature? Maybe you want to tell us about some bug...',
-          'Or maybe there is some accessibility issue? Anyway, contact us using the form bellow.',
+          "Do you want a new feature? Maybe you want to tell us about some bug...",
+          "Or maybe there is some accessibility issue? Anyway, contact us using the form bellow.",
         ]}
       />
-      <Form>
+
+      <Form hasError={hasError}>
+        {hasSent && (
+          <div>
+            <h4>Thank you for your contact!</h4>
+            <p>We will answer as soon as we can!</p>
+          </div>
+        )}
         <div>
           <label>
             Name
-            <input type="text" />
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              disabled={hasSent}
+            />
+            {errors.includes('name') && <span>Please answer the name</span>}
           </label>
         </div>
         <div>
           <label>
             Subject
-            <input type="text" />
+            <input
+              type="text"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+              disabled={hasSent}
+            />
+            {errors.includes('subject') && <span>Please answer the subject</span>}
           </label>
         </div>
         <div>
           <label>
             E-mail
-            <input type="text" />
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              disabled={hasSent}
+            />
+            {errors.includes('email') && <span>Please answer a valid e-mail</span>}
           </label>
         </div>
         <div>
           <label>
             Message
-            <textarea />
+            <textarea
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              disabled={hasSent}
+            />
+            {errors.includes('message') && <span>The message is too short</span>}
           </label>
         </div>
-        <div>
-          <button>Send</button>
-        </div>
+        {!hasSent && (
+          <div>
+            <button onClick={(e) => handleFormSubmition(e)}>
+              {hasError ? `Please fix the errors before submitting` : `Send`}
+            </button>
+          </div>
+        )}
       </Form>
     </PageTemplate>
   );
@@ -57,6 +141,13 @@ const Form = styled.form`
   line-height: 1.5;
   color: #212529;
   text-align: left;
+  h4 {
+    text-align: center;
+  }
+  p {
+    text-align: center;
+    margin-bottom: 1rem;
+  }
   div {
     margin-bottom: 1rem;
     width: 100%;
@@ -64,6 +155,10 @@ const Form = styled.form`
       width: 100%;
       cursor: default;
       margin-bottom: 0.5rem;
+      span {
+        color: red;
+        font-size: .8rem;
+      }
     }
     input,
     textarea {
@@ -89,17 +184,19 @@ const Form = styled.form`
       display: block;
       width: 100%;
       color: #fff;
-      background-color: #007bff;
+      background-color: ${(props) => (props.hasError ? "red" : "#007bff")};
       border-color: #007bff;
       font-weight: 400;
       text-align: center;
       border: 1px solid transparent;
       padding: 0.375rem 0.75rem;
+      margin: 2px;
       font-size: 1rem;
       line-height: 1.5;
       border-radius: 0.25rem;
       &:hover {
-        background-color: #007bffdd;
+        background-color: ${(props) =>
+          props.hasError ? "#bb0000" : "#007bffdd"};
       }
     }
   }
